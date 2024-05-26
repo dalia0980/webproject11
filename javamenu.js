02238d142e769e2be118964a338426ee1eda7e02
dalia0980 <najlaa.alqa74@gmail.com>
2024-05-26 19:34:53 +0300

    Add files via upload

---
+++ javamenu.js
@@ -0,0 +1,17 @@
+function goToSalads() {
+    window.location.href = "salads.html";
+}
+
+
+function goToPizza() {
+     window.location.href = "pizza.html" ;
+}
+
+
+function goToPstaa() {
+     window.location.href = "pasta.html";
+}
+
+function goToDrinks() {
+   window.location.href = "drinks.html";
+        }
